33362950fd84cee8a5777437d8662e28000fc805
logan81000 <logan81100@gmail.com>
2024-05-19 00:28:35 -0400

    Add files via upload

---
+++ Indiv_List.java
@@ -0,0 +1,13 @@
+package PROGRAMMING_1;
+
+public class Indiv_List {
+	public static void main(String[] arg) {
+		System.out.println("David");
+		System.out.println("Valitin");
+		System.out.println("562 Letterman St");
+		System.out.println("Garytown");
+		System.out.println("42598");
+		
+	}
+
+}
